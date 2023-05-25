82204c4951b2a5857e17f402b934b5e455b4d2ef
andre Lameiras <afplameiras@gmail.com>
2023-05-25 18:09:36 +0100

    Criar funçao para converter um acessorio interior em ContentValues

--- app/src/main/java/pt/ipg/acessoriosautomoveis/AcessInter.kt
+++ app/src/main/java/pt/ipg/acessoriosautomoveis/AcessInter.kt
@@ -1,9 +1,22 @@
 package pt.ipg.acessoriosautomoveis
 
+import android.content.ContentValues
+
 data class AcessInter(
     var nome: String,
     var marca: String,
     var descricao: String,
     var id: Long = -1
 ) {
+
+    fun toContentValues() : ContentValues {
+        val valores = ContentValues()
+
+        valores.put(TabelaAcesInter.CAMPO_NOME, nome)
+        valores.put(TabelaAcesInter.CAMPO_MARCA, marca)
+        valores.put(TabelaAcesInter.CAMPO_DESCRICAO, descricao)
+
+        return valores
+    }
+
 }
--- app/src/main/java/pt/ipg/acessoriosautomoveis/TabelaAcesInter.kt
+++ app/src/main/java/pt/ipg/acessoriosautomoveis/TabelaAcesInter.kt
@@ -6,12 +6,15 @@ import android.database.sqlite.SQLiteDatabase
 
 class TabelaAcesInter(db: SQLiteDatabase): TabelaDB(db, NOME_TABELA) {
     override fun cria() {
-        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, nome TEXT NOT NULL, marca TEXT NOT NULL, descricao TEXT NOT NULL)")
+        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, $CAMPO_NOME TEXT NOT NULL, $CAMPO_MARCA TEXT NOT NULL, $CAMPO_DESCRICAO TEXT NOT NULL)")
 
     }
 
     companion object {
         const val NOME_TABELA = "acessoriosInterior"
+        const val CAMPO_NOME = "nome"
+        const val CAMPO_MARCA = "marca"
+        const val CAMPO_DESCRICAO = "descricao"
     }
 
 }
