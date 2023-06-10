053b05e34a07c06226037ad3fc781fb977742ac9
andre Lameiras <afplameiras@gmail.com>
2023-06-10 16:29:11 +0100

    Criada uma funçao para obter um acessorio de exterior a partir de um cursor

--- app/src/main/java/pt/ipg/acessoriosautomoveis/AcessExter.kt
+++ app/src/main/java/pt/ipg/acessoriosautomoveis/AcessExter.kt
@@ -1,6 +1,8 @@
 package pt.ipg.acessoriosautomoveis
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class AcessExter(
     var nome: String,
@@ -19,4 +21,23 @@ data class AcessExter(
         return valores
 
     }
+
+
+    companion object {
+        fun fromCursor(cursor: Cursor): AcessInter{
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posNome = cursor.getColumnIndex(TabelaAcesExter.CAMPO_NOME)
+            val posMarca = cursor.getColumnIndex(TabelaAcesExter.CAMPO_MARCA)
+            val posCor = cursor.getColumnIndex(TabelaAcesExter.CAMPO_COR)
+
+            val id = cursor.getLong(posId)
+            val nome = cursor.getString(posNome)
+            val marca = cursor.getString(posMarca)
+            val cor = cursor.getString(posCor)
+
+            return AcessInter(nome, marca, cor, id)
+
+        }
+    }
+
 }
