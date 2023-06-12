0810c8b7bab4193871e11490978bbb5a9197a537
andre Lameiras <afplameiras@gmail.com>
2023-06-12 10:19:46 +0100

    Criado UriMatcher para validar os endereços do content provider

--- app/src/main/java/pt/ipg/acessoriosautomoveis/CarroContentProvider.kt
+++ app/src/main/java/pt/ipg/acessoriosautomoveis/CarroContentProvider.kt
@@ -2,6 +2,7 @@ package pt.ipg.acessoriosautomoveis
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 
@@ -43,5 +44,25 @@ class CarroContentProvider : ContentProvider(){
         selectionArgs: Array<out String>?
     ): Int {
         TODO("Not yet implemented")
+    }
+
+
+
+    companion object{
+        private const val AUTORIDADE = "pt.ipg.carros"
+
+        const val ACESSINTER = "acessorios de interior"
+        const val ACESSEXTER = "acessorios de exterior"
+        const val CARROS = "carros"
+
+        private const val URI_ACESSINTER = 100
+        private const val URI_ACESSEXTER = 200
+        private const val URI_CARROS = 300
+
+        fun uriMatcher() = UriMatcher(UriMatcher.NO_MATCH).apply {
+            addURI(AUTORIDADE, ACESSINTER, URI_ACESSINTER)
+            addURI(AUTORIDADE, ACESSEXTER, URI_ACESSEXTER)
+            addURI(AUTORIDADE, CARROS, URI_CARROS)
+        }
     }
 }
