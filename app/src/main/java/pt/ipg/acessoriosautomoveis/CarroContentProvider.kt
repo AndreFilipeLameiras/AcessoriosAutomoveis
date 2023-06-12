63cecb9db118f75badad7bbd197d2a62cabb27d3
andre Lameiras <afplameiras@gmail.com>
2023-06-12 10:32:47 +0100

    Implementado o metodo query no content provider

--- app/src/main/java/pt/ipg/acessoriosautomoveis/CarroContentProvider.kt
+++ app/src/main/java/pt/ipg/acessoriosautomoveis/CarroContentProvider.kt
@@ -5,6 +5,7 @@ import android.content.ContentValues
 import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
+import android.provider.BaseColumns
 
 class CarroContentProvider : ContentProvider(){
     private var bdOpenHelper: BdAcessoriosOpenHelper? = null
@@ -22,7 +23,32 @@ class CarroContentProvider : ContentProvider(){
         selectionArgs: Array<out String>?,
         sortOrder: String?
     ): Cursor? {
-        TODO("Not yet implemented")
+        val bd = bdOpenHelper!!.readableDatabase
+
+        val endereco = uriMatcher().match(uri)
+        val tabela = when (endereco){
+            URI_ACESSINTER, URI_ACESSINTER_ID -> TabelaAcesInter(bd)
+            URI_ACESSEXTER, URI_ACESSEXTER_ID -> TabelaAcesExter(bd)
+            URI_CARROS, URI_CARROS_ID -> TabelaCarro(bd)
+            else -> null
+        }
+
+        val id = uri.lastPathSegment
+
+        val (selecao, argsSel) = when (endereco){
+            URI_ACESSINTER_ID, URI_CARROS_ID -> Pair("${BaseColumns._ID}=?", arrayOf(id))
+            URI_ACESSEXTER_ID, URI_CARROS_ID -> Pair("${BaseColumns._ID}=?", arrayOf(id))
+            else -> Pair(selection, selectionArgs)
+        }
+
+        return tabela?.consulta(
+            projection as Array<String>,
+            selecao,
+            argsSel as Array<String?>,
+            null,
+            null,
+            sortOrder
+        )
     }
 
     override fun getType(uri: Uri): String? {
@@ -56,13 +82,19 @@ class CarroContentProvider : ContentProvider(){
         const val CARROS = "carros"
 
         private const val URI_ACESSINTER = 100
+        private const val URI_ACESSINTER_ID = 101
         private const val URI_ACESSEXTER = 200
+        private const val URI_ACESSEXTER_ID = 201
         private const val URI_CARROS = 300
+        private const val URI_CARROS_ID = 301
 
         fun uriMatcher() = UriMatcher(UriMatcher.NO_MATCH).apply {
             addURI(AUTORIDADE, ACESSINTER, URI_ACESSINTER)
+            addURI(AUTORIDADE, "$ACESSINTER/#", URI_ACESSINTER_ID)
             addURI(AUTORIDADE, ACESSEXTER, URI_ACESSEXTER)
+            addURI(AUTORIDADE, "$ACESSEXTER/#", URI_ACESSEXTER_ID)
             addURI(AUTORIDADE, CARROS, URI_CARROS)
+            addURI(AUTORIDADE, "$CARROS/#", URI_CARROS_ID)
         }
     }
 }
