7d6a47e93199ebb36a6181e4e1f6b0be92b5b377
andre Lameiras <afplameiras@gmail.com>
2023-06-15 10:45:26 +0100

    Navegar entre a lista de carros e o novo carro

--- app/src/main/java/pt/ipg/acessoriosautomoveis/ListaCarrosFragment.kt
+++ app/src/main/java/pt/ipg/acessoriosautomoveis/ListaCarrosFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import pt.ipg.acessoriosautomoveis.databinding.FragmentListaCarrosBinding
 
@@ -188,7 +189,7 @@ class ListaCarrosFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
     }
 
     private fun adicionaCarro() {
-        TODO("Not yet implemented")
+        findNavController().navigate(R.id.action_listaCarrosFragment_to_novoCarroFragment)
     }
 
 }
