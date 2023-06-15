ca041b6d5f89699e4caf6682a7c04edd8bf9fb14
andre Lameiras <afplameiras@gmail.com>
2023-06-15 10:28:08 +0100

    Adicionado fragmento para criar um novo carro

---
+++ app/src/main/java/pt/ipg/acessoriosautomoveis/NovoCarroFragment.kt
@@ -0,0 +1,50 @@
+package pt.ipg.acessoriosautomoveis
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pt.ipg.acessoriosautomoveis.databinding.FragmentNovoCarroBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [NovoCarroFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class NovoCarroFragment : Fragment() {
+    private var _binding: FragmentNovoCarroBinding? = null
+
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentNovoCarroBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = activity as MainActivity
+        activity.fragment = this
+        activity.idMenuAtual = R.menu.menu_main
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
