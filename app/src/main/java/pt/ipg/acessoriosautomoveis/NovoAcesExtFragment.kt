da27c58b1c607caf2ff53505cfbdc652cb0813d5
andre Lameiras <afplameiras@gmail.com>
2023-06-19 15:48:04 +0100

    Guardar acessorios

--- app/src/main/java/pt/ipg/acessoriosautomoveis/NovoAcesExtFragment.kt
+++ app/src/main/java/pt/ipg/acessoriosautomoveis/NovoAcesExtFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import pt.ipg.acessoriosautomoveis.databinding.FragmentNovoAcesExtBinding
 
@@ -47,18 +48,56 @@ class NovoAcesExtFragment : Fragment() {
                 true
             }
             R.id.action_cancelar -> {
-                cancelar()
+                voltaListaAcesExt()
                 true
             }
             else -> false
         }
     }
 
-    private fun cancelar() {
+    private fun voltaListaAcesExt() {
         findNavController().navigate(R.id.action_novoAcesExtFragment_to_listaAcessExtFragment)
     }
 
     private fun guardar() {
+        val nome = binding.editTextNomeExt.text.toString()
+        if (nome.isBlank()){
+            binding.editTextNomeExt.error = getString(R.string.nome_obrigatorio)
+            binding.editTextNomeExt.requestFocus()
+            return
+        }
 
+        val categoria = binding.editTextMarcaExt.text.toString()
+        if (categoria.isBlank()){
+            binding.editTextMarcaExt.error = getString(R.string.categoria_obrigatoria)
+            binding.editTextMarcaExt.requestFocus()
+            return
+        }
+
+        val cor = binding.ediTextCorExt.text.toString()
+        if( cor.isBlank()){
+            binding.ediTextCorExt.error = getString(R.string.cor_obrigatoria)
+            binding.ediTextCorExt.requestFocus()
+            return
+        }
+
+
+        val acessExter = AcessExter(
+            nome,
+            categoria,
+            cor
+        )
+
+        val id = requireActivity().contentResolver.insert(
+            CarroContentProvider.ENDERECO_ACESSEXTER,
+            acessExter.toContentValues()
+        )
+
+        if (id == null){
+            binding.editTextNomeExt.error = getString(R.string.erro_guadar_acessorio_exterior)
+            return
+        }
+        Toast.makeText(requireContext(), getString(R.string.acessorio_ext_guardado_com_sucesso), Toast.LENGTH_SHORT).show()
+        voltaListaAcesExt()
     }
 }
--- app/src/main/java/pt/ipg/acessoriosautomoveis/NovoAcessInteriorFragment.kt
+++ app/src/main/java/pt/ipg/acessoriosautomoveis/NovoAcessInteriorFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import pt.ipg.acessoriosautomoveis.databinding.FragmentNovoAcessInteriorBinding
 
@@ -46,18 +47,58 @@ class NovoAcessInteriorFragment : Fragment() {
                 true
             }
             R.id.action_cancelar -> {
-                cancelar()
+                voltaListaAcesInter()
                 true
             }
             else -> false
         }
     }
 
-    private fun cancelar() {
+    private fun voltaListaAcesInter() {
         findNavController().navigate(R.id.action_novoAcessInteriorFragment_to_listaAcessInteriorFragment)
     }
 
     private fun guardar() {
+        val nome = binding.editTextNomeInterior.text.toString()
+        if (nome.isBlank()){
+            binding.editTextNomeInterior.error = getString(R.string.nome_obrigatorio)
+            binding.editTextNomeInterior.requestFocus()
+            return
+        }
+
+        val classe = binding.editTextMarcaInterior.text.toString()
+        if (classe.isBlank()){
+            binding.editTextMarcaInterior.error = getString(R.string.classe_obrigatoria)
+            binding.editTextMarcaInterior.requestFocus()
+            return
+        }
+
+        val descricao = binding.editTextDescricao.text.toString()
+        if( descricao.isBlank()){
+            binding.editTextDescricao.error = getString(R.string.descricao_obrigatoria)
+            binding.editTextDescricao.requestFocus()
+            return
+        }
+
+
+        val acessInter = AcessInter(
+            nome,
+            classe,
+            descricao
+        )
+
+        val id = requireActivity().contentResolver.insert(
+            CarroContentProvider.ENDERECO_ACESSiNTER,
+            acessInter.toContentValues()
+        )
+
+        if (id == null){
+            binding.editTextNomeInterior.error = getString(R.string.erro_guadar_acessorio_interior)
+            return
+        }
+        Toast.makeText(requireContext(), getString(R.string.acessorio_int_guardado_com_sucesso), Toast.LENGTH_SHORT).show()
+        voltaListaAcesInter()
+
 
     }
 
